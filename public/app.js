238e8fff5d20d665b69ed3c53971dc95411de766
Eoghan Crowley <eoghanscrowley@gmail.com>
2017-06-22 19:02:13 +0100

    refactor

--- public/app.js
+++ public/app.js
@@ -1,56 +1,67 @@
-var currentPosition = {x: 120, y: 100, xKnobRotation: 0, yKnobRotation: 0};
 
 var app = function() {
 	var canvas = document.getElementById('main-canvas');
 	var ctx = canvas.getContext('2d');
+
+	var currentPosition = {x: 120, y: 100, xKnobRotation: 0, yKnobRotation: 0};
 	ctx.moveTo(currentPosition.x, currentPosition.y);
 
 	var rotateKnob = function(axis, movement) {
-		var id = axis + "-knob";
-		var knob = document.getElementById(id);
-		var knobKey = axis + "KnobRotation"
+		var knob = document.getElementById(axis + '-knob');
+		var knobKey = axis + 'KnobRotation';
 		currentPosition[knobKey] += movement;
-		knob.setAttribute('style', 'transform: rotate(' + currentPosition[axis + "KnobRotation"] + "deg);");
+		var styleString = 'transform: rotate(' + currentPosition[knobKey] + 'deg);';
+		knob.setAttribute('style', styleString);
 	}
-	var drawLine = function(axis, movement) {
-		var nextX, nextY;
+
+	var calculateNext = function(axis, movement) {
+		var next = {};
 		if (axis === 'x') {
-			var nextX = currentPosition.x + movement;
-			var nextY = currentPosition.y;
+			next.x = currentPosition.x + movement;
+			next.y = currentPosition.y;
 		} else {
-			var nextX = currentPosition.x;
-			var nextY = currentPosition.y + movement;
+			next.x = currentPosition.x;
+			next.y = currentPosition.y + movement;
 		}
-		
-		if (nextX > 0 && nextX < 500 && nextY > 0 && nextY < 400) {
+		return next;
+	}
+
+	var drawLine = function(axis, movement) {
+		var next = calculateNext(axis, movement);
+		if (next.x > 0 && next.x < 500 && next.y > 0 && next.y < 400) {
 			rotateKnob(axis, movement);
 			ctx.beginPath();		
 			ctx.moveTo(currentPosition.x, currentPosition.y);
-			currentPosition.x = nextX;
-			currentPosition.y = nextY;	
+			currentPosition.x = next.x;
+			currentPosition.y = next.y;	
 			ctx.lineTo(currentPosition.x, currentPosition.y);
 			ctx.stroke();
-		}
-		
+		}	
 	}
+
 	var handleMove = function(event) {
 		event.preventDefault();
-		var keyCode = event.which;
+		var keyCode = event.keyCode;
 		if (keyCode === 38) drawLine('y', -2);
 		if (keyCode === 40) drawLine('y', 2);
 		if (keyCode === 37) drawLine('x', -2);
 		if (keyCode === 39) drawLine('x', 2);
 	}
-	window.addEventListener('keydown', handleMove);
-	var resetButton = document.getElementById("reset");
-	ctx.fillStr
+
 	var handleReset = function() {
 		ctx.beginPath();
 		ctx.clearRect(0, 0, 600, 500)
-		currentPosition = {x: 300, y: 250, xKnobRotation: currentPosition.xKnobRotation, yKnobRotation: currentPosition.yKnobRotation};
-
+		currentPosition = {
+			x: 120,
+			y: 100,
+			xKnobRotation: currentPosition.xKnobRotation,
+			yKnobRotation: currentPosition.yKnobRotation
+		}
 	}
+
+	var resetButton = document.getElementById('reset');
 	resetButton.addEventListener('click', handleReset);
+	window.addEventListener('keydown', handleMove);
 	window.focus();
 }
 
